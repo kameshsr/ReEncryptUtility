a99473a61e5999dfb76826940c75a50a42ad507f
kameshsr <kameshsr1338@gmail.com>
2022-03-10 12:12:46 +0530

    Updated Re-encrypted data

--- src/main/java/com/ReEncryptUtility/ReEncryptUtility/repository/ReEncrptyRepository.java
+++ src/main/java/com/ReEncryptUtility/ReEncryptUtility/repository/ReEncrptyRepository.java
@@ -4,8 +4,6 @@ import com.ReEncryptUtility.ReEncryptUtility.entity.DemographicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-//repository that extends CrudRepository
-
 public interface ReEncrptyRepository extends JpaRepository<DemographicEntity, String> {
-
+    public DemographicEntity findBypreRegistrationId(@Param("preRegId") String preRegId);
 }
--- src/main/java/com/ReEncryptUtility/ReEncryptUtility/service/ReEncrypt.java
+++ src/main/java/com/ReEncryptUtility/ReEncryptUtility/service/ReEncrypt.java
@@ -8,6 +8,7 @@ import com.ReEncryptUtility.ReEncryptUtility.entity.DemographicEntity;
 import com.ReEncryptUtility.ReEncryptUtility.repository.ReEncrptyRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.mosip.kernel.core.util.HMACUtils;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -90,78 +91,6 @@ public class ReEncrypt {
         }));
     }
 
-//    public String reEncryptDatabaseValues(String query) throws SQLException {
-//        logger.info("PostgreSQL JDBC Connection Testing ~");
-//
-//        try (Connection connection = getConnection()) {
-//            Statement statement = connection.createStatement();
-//            ResultSet rs = statement.executeQuery(query);
-//            row = 0;
-//            while (rs.next()&& row<3) {
-//                logger.info("row=: " + row++);
-//                logger.info("Pre_Reg_ID = " + rs.getString("prereg_id"));
-//                byte[] demog_details = rs.getBytes("demog_detail");
-//                logger.info("Encrypted demog_detail=\n" + new String(demog_details));
-//                logger.info("demog_detail_hash\n" + rs.getString("demog_detail_hash"));
-//                logger.info("account:-" + rs.getString("cr_by"));
-//                if (demog_details.length > 0) {
-//                    byte[] decrypted = decrypt(demog_details, LocalDateTime.now(), decryptBaseUrl);
-//                    if (decrypted == null) {
-//                        logger.info("Decrypted data is null");
-//                        continue;
-//                    }
-//                    logger.info("decrypted pre-reg-data-:-\n" + new String(decrypted));
-//                    byte[] ReEncrypted = encrypt(decrypted, LocalDateTime.now(), encryptBaseUrl);
-//                    logger.info("ReEncrypted pre-reg-data-:-\n" + new String(ReEncrypted));
-//
-//                    try {
-//                        String updateQuery =
-//                                "update applicant_demographic set demog_detail=?, demog_detail_hash=?, encrypted_dtimes=?::timestamp where prereg_id=?";
-//                        PreparedStatement stmt = targetDatabaseConnection.prepareStatement(updateQuery);
-//                        stmt.setBytes(1, ReEncrypted);
-//                        stmt.setString(2, hashUtill(ReEncrypted));
-//                        LocalDateTime encryptionDateTime = DateUtils.getUTCCurrentDateTime();
-//                        stmt.setTimestamp(3, Timestamp.valueOf(encryptionDateTime));
-//                        stmt.setString(4, rs.getString("prereg_id"));
-//                        int checkUpdateStatus = stmt.executeUpdate();
-//                        if (checkUpdateStatus > 0) {
-//                            logger.info("successfully updated");
-//                        } else {
-//                            logger.info("inserting new record");
-//                            String insertQuery = "INSERT INTO applicant_demographic(prereg_id,demog_detail,demog_detail_hash," +
-//                                    "encrypted_dtimes, status_code, lang_code, cr_appuser_id, cr_by, cr_dtimes, upd_by, upd_dtimes) "
-//                                    + "VALUES(?,?,?,?::timestamp,?,?,?,?,?::timestamp,?,?::timestamp)";
-//                            PreparedStatement stmt1 = targetDatabaseConnection.prepareStatement(insertQuery);
-//                            stmt1.setString(1, rs.getString("prereg_id"));
-//                            stmt1.setBytes(2, ReEncrypted);
-//                            stmt1.setString(3, hashUtill(ReEncrypted));
-//                            stmt1.setString(4, String.valueOf(Timestamp.valueOf(encryptionDateTime)));
-//                            stmt1.setString(5, rs.getString("status_code"));
-//                            stmt1.setString(6, rs.getString("lang_code"));
-//                            stmt1.setString(7, rs.getString("cr_appuser_id"));
-//                            stmt1.setString(8, rs.getString("cr_by"));
-//                            stmt1.setString(9, rs.getString("cr_dtimes"));
-//                            stmt1.setString(10, rs.getString("upd_by"));
-//                            stmt1.setString(11, rs.getString("upd_dtimes"));
-//                            int checkInsertStatus = stmt1.executeUpdate();
-//                            if (checkInsertStatus > 0) {
-//                                logger.info("successfully inserted");
-//
-//                            }
-//                        }
-//                        successFullRow++;
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//            logger.info("Total rows=: " + row);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return "Success";
-//    }
-
     public byte[] decrypt(byte[] originalInput, LocalDateTime localDateTime, String decryptBaseUrl) throws Exception {
         logger.info("In decrypt method of CryptoUtil service ");
         ResponseEntity<ResponseWrapper<CryptoManagerResponseDTO>> response = null;
@@ -219,9 +148,9 @@ public class ReEncrypt {
         return encryptedBytes;
     }
 
-//    public static String hashUtill(byte[] bytes) {
-//        return HMACUtils.digestAsPlainText(HMACUtils.generateHash(bytes));
-//    }
+    public static String hashUtill(byte[] bytes) {
+        return HMACUtils.digestAsPlainText(HMACUtils.generateHash(bytes));
+    }
 
     @Autowired
     private ReEncrptyRepository reEncrptyRepository;
@@ -235,8 +164,8 @@ public class ReEncrypt {
     private void reEncryptData(List<DemographicEntity> applicantDemographic) throws Exception {
         int count = 0;
         for (DemographicEntity demographicEntity : applicantDemographic) {
-            if (count++ > 5)
-                break;
+//            if (count++ > 5)
+//                break;
             System.out.println("pre registration id: " + demographicEntity.getPreRegistrationId());
             System.out.println("encrypted : " + new String(demographicEntity.getApplicantDetailJson()));
             if (demographicEntity.getApplicantDetailJson() != null) {
@@ -246,6 +175,12 @@ public class ReEncrypt {
                 System.out.println("decrypted: " + new String(decryptedBytes));
                 byte[] ReEncrypted = encrypt(decryptedBytes, LocalDateTime.now(), encryptBaseUrl);
                 System.out.println("ReEncrypted: " + new String(ReEncrypted));
+                DemographicEntity demographicEntity1 = reEncrptyRepository.findBypreRegistrationId(demographicEntity.getPreRegistrationId());
+                demographicEntity1.setApplicantDetailJson(ReEncrypted);
+                demographicEntity1.setEncryptedDateTime(LocalDateTime.now());
+                demographicEntity1.setDemogDetailHash(hashUtill(ReEncrypted));
+                reEncrptyRepository.save(demographicEntity1);
+                System.out.println(demographicEntity1.getEncryptedDateTime());
             }
         }
     }
